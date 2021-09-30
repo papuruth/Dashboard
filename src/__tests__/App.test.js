4c4e71314fcd4f696a743995e66ec0e11cd4f26a
Papu Kumar <papu.kumar@kelltontech.com>
2021-09-30 21:44:20 +0530

    #added test for app component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,2 +1,9 @@
+import App from '@/App';
+import { render } from '@testing-library/react';
 
-test("renders learn react link", () => {});
+describe('testing App.js component', () => {
+  test('should mount app component', () => {
+    const wrapper = render(<App />);
+    expect(wrapper).toMatchSnapshot()
+  });
+});
